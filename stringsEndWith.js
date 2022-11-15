9e4fc2e8b6358afdeb6d5e4c4f8910a9a1c96cc4
felixc0115 <85801744+felixc0115@users.noreply.github.com>
2022-11-14 21:59:49 -0500

    Update stringsEndWith.js

--- stringsEndWith.js
+++ stringsEndWith.js
@@ -1,3 +1,13 @@
+DESCRIPTION:
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+
+
 function solution(str, ending) {
   let strArray = str.split("");
   let resultArray = [];
