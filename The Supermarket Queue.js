f8b82e9d1fc926b5d82a220f123655d34c397e86
felixc0115 <85801744+felixc0115@users.noreply.github.com>
2022-12-19 18:15:35 -0500

    Create The Supermarket Queue.js

---
+++ The Supermarket Queue.js
@@ -0,0 +1,8 @@
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+// input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// output
+// The function should return an integer, the total time required.
+
