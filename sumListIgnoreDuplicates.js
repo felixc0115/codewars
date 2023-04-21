18e0a95cb9ea9291cb787d5912ff6d2b335278a7
felixc0115 <fechen1993@gmail.com>
2023-04-20 22:21:57 -0400

    Update sumListIgnoreDuplicates.js

--- sumListIgnoreDuplicates.js
+++ sumListIgnoreDuplicates.js
@@ -3,3 +3,23 @@ Please write a function that sums a list, but ignores any duplicate items in the
 
 For instance, for the list [3, 4, 3, 6] , the function should return 10.
 */
+
+function sumNoDuplicates(numList) {
+ const numObject = {}
+ const uniqueList = []
+ numList.forEach((num) => {
+   if(!numObject[num]) {
+     numObject[num] = 1
+   }
+   else {
+     numObject[num] ++ 
+   }
+ })
+  for (const num in numObject) {
+    if(numObject[num] === 1) {
+      uniqueList.push(+num)
+    }
+  }
+  let totalSum = uniqueList.reduce((total, curr) => total + curr, 0)
+  return totalSum
+}
