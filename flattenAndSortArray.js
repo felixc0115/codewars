be05aa93cb32a7612e579581d703e969528689a7
felixc0115 <fechen1993@gmail.com>
2023-04-24 19:33:55 -0400

    Update flattenAndSortArray.js

--- flattenAndSortArray.js
+++ flattenAndSortArray.js
@@ -8,3 +8,14 @@ Example:
 Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 */
 
+
+function flattenAndSort(array) {
+  let completeArray = []
+ array.forEach((numArray) => {
+   numArray.forEach((num) => {
+     console.log(num)
+     if(typeof num === 'number')  completeArray.push(num)
+   })
+ })
+  return completeArray.sort((a,b) => a - b);
+}
