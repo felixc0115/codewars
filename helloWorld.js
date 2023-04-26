ad8940c0638737b830325b5f689f715b7b5e8b1d
felixc0115 <fechen1993@gmail.com>
2023-04-25 22:53:34 -0400

    Create helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,5 @@
+/* 
+You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+
+*/
