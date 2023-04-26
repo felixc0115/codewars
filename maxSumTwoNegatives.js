b2fef83859f6ce1ed24abb96d58eac1e3a43d12a
felixc0115 <fechen1993@gmail.com>
2023-04-26 18:51:52 -0400

    Create maxSumTwoNegatives.js

---
+++ maxSumTwoNegatives.js
@@ -0,0 +1,15 @@
+/* 
+  
+DESCRIPTION:
+Task
+You have a list of integers. The task is to return the maximum sum of the elements located between two negative elements. No negative element should be present in the sum. If there is no such sum: -1 for Python, JavaScript and COBOL, Nothing for Haskell, None for Rust.
+
+Example
+[4, -1, 6, -2, 3, 5, -7, 7] -> 8
+     ^      ^         ^
+Sum between -1 and -2 is 6, between -2 and -7 is 3 + 5 = 8. It's also not 14 (between -1 and -7), because this includes a negative number (-2).
+
+  
+  
+*/
+
