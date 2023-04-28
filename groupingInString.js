8c8952e16c5b4642304c9e5b5852b25ea1f96a7c
felixc0115 <fechen1993@gmail.com>
2023-04-28 15:02:08 -0400

    Create groupingInString.js

---
+++ groupingInString.js
@@ -0,0 +1,16 @@
+/* 
+
+In this kata you have to find whether same elements of the string are grouped together.
+
+Example
+
+Input	Output
+112233	true
+11223311	false
+
+Explanation
+In first example same elements are grouped together: (11)(22)(33), hence it should return true
+In the second example there are two different groups of 1's: (11)2233(11), hence it should return false
+
+
+*/
