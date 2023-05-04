4ed9d704132e4c8a48f7d79d9412e5426026ccf0
felixc0115 <fechen1993@gmail.com>
2023-05-04 18:44:06 -0400

    Update whichAreIn.js

--- whichAreIn.js
+++ whichAreIn.js
@@ -19,3 +19,15 @@
 // Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
 // In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
 // Beware: In some languages r must be without duplicates.
+
+function inArray(array1,array2){
+  let substringArray = []
+  array1.forEach((word1) => {
+    array2.forEach((word2) => {
+      if(word2.includes(word1) && !substringArray.includes(word1)){
+         substringArray.push(word1)
+      }
+    } )
+  })
+  return substringArray.sort()
+}
