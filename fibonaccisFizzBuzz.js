a49390334b27c8cae89a9af1e3273e4799fafe61
felixc0115 <fechen1993@gmail.com>
2023-05-08 09:54:05 -0500

    Create fibonaccisFizzBuzz.js

---
+++ fibonaccisFizzBuzz.js
@@ -0,0 +1,10 @@
+/*
+The goal of this kata is two-fold:
+
+1.) You must produce a fibonacci sequence in the form of an array, containing a number of items equal to the input provided.
+
+2.) You must replace all numbers in the sequence divisible by 3 with Fizz, those divisible by 5 with Buzz, and those divisible by both 3 and 5 with FizzBuzz.
+
+For the sake of this kata, you can assume all input will be a positive integer.
+
+*/
