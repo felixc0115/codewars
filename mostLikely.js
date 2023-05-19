33ae3ea719cbcc09f9d2667ae0f82e94d1a54503
felixc0115 <fechen1993@gmail.com>
2023-05-18 21:08:21 -0400

    Create mostLikely.js

---
+++ mostLikely.js
@@ -0,0 +1,7 @@
+/*
+
+Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+*/
