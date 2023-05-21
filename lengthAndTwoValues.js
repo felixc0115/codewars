b8b8039ef13eee092c9fd306adabe50d90993d98
felixc0115 <fechen1993@gmail.com>
2023-05-20 21:16:34 -0400

    Update lengthAndTwoValues.js

--- lengthAndTwoValues.js
+++ lengthAndTwoValues.js
@@ -1,3 +1,11 @@
 /*
 Given an integer n and two other values, build an array of size n filled with these two values alternating.
 */
+
+function alternate(n, firstValue, secondValue){
+  const arr = []
+  for (let i=1;i<=n;i++) {
+    arr.push(i % 2 === 0 ? secondValue: firstValue)
+  }
+  return arr
+}
